7fdac4197868135648eec103f06edac1aba810da
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-07-22 19:06:57 +0200

    directives d attributs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { POKEMON_LIST } from './pokemon-list.fake';
 import { Pokemon } from './pokemon.model';
+import { PokemonBorderDirective } from './pokemon-border.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PokemonBorderDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
