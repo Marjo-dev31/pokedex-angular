7440ae65509edd88c82cdfa243d3877381c09dfb
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-10-04 15:16:33 +0200

    form validation

--- src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -2,15 +2,20 @@ import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
 import { DatePipe, JsonPipe } from '@angular/common';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { getPokemonColor } from '../../pokemon.model';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule, Validators
+} from '@angular/forms';
+import { POKEMON_RULES, getPokemonColor } from '../../pokemon.model';
 
 @Component({
   selector: 'app-pokemon-edit',
   standalone: true,
   imports: [DatePipe, RouterLink, ReactiveFormsModule, JsonPipe],
   templateUrl: './pokemon-edit.component.html',
-  styles: ``
+  styles: ``,
 })
 export class PokemonEditComponent {
   readonly route = inject(ActivatedRoute);
@@ -19,46 +24,87 @@ export class PokemonEditComponent {
   readonly pokemon = signal(
     this.pokemonService.getPokemonById(this.pokemonId)
   ).asReadonly();
+  readonly POKEMON_RULES = signal(POKEMON_RULES).asReadonly();
 
   readonly form = new FormGroup({
-    name: new FormControl(this.pokemon().name),
+    name: new FormControl(this.pokemon().name, [
+      Validators.required,
+      Validators.minLength(POKEMON_RULES.MIN_NAME),
+      Validators.maxLength(POKEMON_RULES.MAX_NAME),
+      Validators.pattern(POKEMON_RULES.NAME_PATTERN),
+    ]),
     life: new FormControl(this.pokemon().life),
     damage: new FormControl(this.pokemon().damage),
-    types: new FormArray(this.pokemon().types.map((type)=> new FormControl(type)))  
-  })
+    types: new FormArray(
+      this.pokemon().types.map((type) => new FormControl(type)), [
+        Validators.required,
+        Validators.maxLength(POKEMON_RULES.MAX_TYPES)
+      ]
+    ),
+  });
 
   // Get the selected Pokemon list by user.
-get pokemonTypeList(): FormArray {
-  return this.form.get('types') as FormArray;
-}
-
-// Return if given type is already selected by user or not.
-isPokemonTypeSelected(type: string): boolean {
-  return !!this.pokemonTypeList.controls.find(
-    (control) => control.value === type
-  );
-}
-
-// Add or remove a given type in the selected Pokemon list.
-onPokemonTypeChange(type: string, isChecked: boolean): void {
-  if (isChecked) {
-    const control = new FormControl(type);
-    this.pokemonTypeList.push(control);
+  get pokemonTypeList(): FormArray {
+    return this.form.get('types') as FormArray;
   }
-  else {
-    const index = this.pokemonTypeList.controls
-      .map((control) => control.value)
-      .indexOf(type);
-    this.pokemonTypeList.removeAt(index);
+
+  // Return if given type is already selected by user or not.
+  isPokemonTypeSelected(type: string): boolean {
+    return !!this.pokemonTypeList.controls.find(
+      (control) => control.value === type
+    );
   }
-}
 
-onSubmit() {
-  console.log(this.form.value);
-}
+  // Add or remove a given type in the selected Pokemon list.
+  onPokemonTypeChange(type: string, isChecked: boolean): void {
+    if (isChecked) {
+      const control = new FormControl(type);
+      this.pokemonTypeList.push(control);
+    } else {
+      const index = this.pokemonTypeList.controls
+        .map((control) => control.value)
+        .indexOf(type);
+      this.pokemonTypeList.removeAt(index);
+    }
+  }
 
-getPokemonColor(type: string) {
-  return getPokemonColor(type);
-}
+  onSubmit() {
+    console.log(this.form.value);
+  }
 
+  getPokemonColor(type: string) {
+    return getPokemonColor(type);
+  }
+
+  get pokemonName() {
+    return this.form.get('name') as FormControl;
+  }
+
+  get pokemonLife() {
+    return this.form.get('life') as FormControl;
+  }
+  
+  incrementLife() {
+    const newValue = this.pokemonLife.value + 1;
+    this.pokemonLife.setValue(newValue);
+  }
+  
+  decrementLife() {
+    const newValue = this.pokemonLife.value - 1;
+    this.pokemonLife.setValue(newValue);
+  }
+
+  get pokemonDamage() {
+    return this.form.get('damage') as FormControl;
+  }
+
+  incrementDamage() {
+    const newValue = this.pokemonDamage.value +1;
+    this.pokemonDamage.setValue(newValue)
+  }
+
+  decrementDamage() {
+    const newValue = this.pokemonDamage.value -1;
+    this.pokemonDamage.setValue(newValue)
+  }
 }
